6e5ff0c6573ddd46a89721539bfe7804f256ce46
INTENCER\k.michalski <krzysztof.michalski@intive.com>
2024-04-05 12:20:19 +0200

    Define Board controller with sketching html in templates for rendering: board (define simple Welcome board message and make sure font-awesome, bootstrap and webjars are working - including them in script tag them), new game creation form (change title)

--- src/main/java/kmichalski/scoreboard/controller/BoardController.java
+++ src/main/java/kmichalski/scoreboard/controller/BoardController.java
@@ -1,2 +1,25 @@
-package kmichalski.scoreboard.controller;public class BoardController {
+package kmichalski.scoreboard.controller;
+
+import kmichalski.scoreboard.model.Game;
+import kmichalski.scoreboard.repostiory.GameRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class BoardController {
+    private final GameRepository gameRepository;
+
+    @RequestMapping(value={"","/","/board"})
+    public String displayBoardPage (Model model) {
+//        Game games =  gameRepository.findById(1L).orElseThrow();
+        List<Game> gamesList =  gameRepository.findAll();
+        return "board.html";
+    }
 }
