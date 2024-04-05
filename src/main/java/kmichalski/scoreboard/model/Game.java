0014e38171de1706cb34159d57a1e52783718116
INTENCER\k.michalski <krzysztof.michalski@intive.com>
2024-04-05 12:12:17 +0200

    Add 2 new columns: homeTeamScore, awayTeamScore in Game entity about match scores. Define EntityGraph for Game entity, associated with Team entity in order to avod N+1 queries problem. Define manyToOne 2 relations to Team entity

--- src/main/java/kmichalski/scoreboard/model/Game.java
+++ src/main/java/kmichalski/scoreboard/model/Game.java
@@ -1,22 +1,51 @@
 package kmichalski.scoreboard.model;
 
 import jakarta.persistence.*;
+import lombok.*;
+
 @Entity
 @Table(name = "games")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
+@NamedEntityGraph(
+        name = "game-entity-graph",
+        attributeNodes = {
+                @NamedAttributeNode(value = "homeTeam", subgraph = "team-subgraph"),
+                @NamedAttributeNode(value = "awayTeam", subgraph = "team-subgraph")
+        },
+        subgraphs = {
+                @NamedSubgraph(
+                        name = "team-subgraph",
+                        attributeNodes = {
+                                @NamedAttributeNode("id"),
+                                @NamedAttributeNode("name") // Add other attributes you want to fetch eagerly
+                        }
+                )
+        }
+)
 public class Game extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name="home_team_id", nullable = false)
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "home_team_id", nullable = false)
     private Team homeTeam;
 
-    @ManyToOne
-    @JoinColumn(name="away_team_id", nullable = false)
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "away_team_id", nullable = false)
     private Team awayTeam;
 
+    @Column
+    private int homeTeamScore;
+
+    @Column
+    private int awayTeamScore;
+
     @Enumerated(EnumType.STRING)
-    private  GameStatus gameStatus;
+    private GameStatus gameStatus;
 }
--- src/main/java/kmichalski/scoreboard/model/Team.java
+++ src/main/java/kmichalski/scoreboard/model/Team.java
@@ -1,9 +1,15 @@
 package kmichalski.scoreboard.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "teams")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
 public class Team extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
