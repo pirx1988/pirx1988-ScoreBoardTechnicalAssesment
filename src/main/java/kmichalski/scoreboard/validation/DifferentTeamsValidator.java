a53a5184e067faa8a1091f106575241f21b025f6
INTENCER\k.michalski <krzysztof.michalski@intive.com>
2024-04-09 13:27:19 +0200

    Renaming GameDto -> newGameDto as a consuquent of using Refactor intellij feature( key shortcut Shift + F6)

--- src/main/java/kmichalski/scoreboard/validation/DifferentTeamsValidator.java
+++ src/main/java/kmichalski/scoreboard/validation/DifferentTeamsValidator.java
@@ -3,12 +3,11 @@ package kmichalski.scoreboard.validation;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import kmichalski.scoreboard.annotation.CheckDifferentTeams;
-import kmichalski.scoreboard.dto.GameDto;
-import kmichalski.scoreboard.model.Game;
+import kmichalski.scoreboard.dto.NewGameDto;
 
-public class DifferentTeamsValidator implements ConstraintValidator<CheckDifferentTeams, GameDto> {
+public class DifferentTeamsValidator implements ConstraintValidator<CheckDifferentTeams, NewGameDto> {
     @Override
-    public boolean isValid(GameDto game, ConstraintValidatorContext context) {
+    public boolean isValid(NewGameDto game, ConstraintValidatorContext context) {
         return game.getHomeTeamId() != game.getAwayTeamId();
     }
 }
